bcebcae0e5e748da85fb54e41744c8e1ea5cb02a
erc <erc@investosaur.com>
2019-07-29 01:15:30 +0800

    add angular materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LyricCardComponent } from './lyric-card/lyric-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LyricCardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
 
+    MatToolbarModule,
+    MatFormFieldModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
